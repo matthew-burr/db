078f3f8fc7222f0d0b1adb34910ae6756d40824d
Matthew Burr <matt.d.burr@gmail.com>
2020-09-05 16:20:49 -0500

    Clean up the interface a bit.

--- main.go
+++ main.go
@@ -24,8 +24,12 @@ func displayInterface(db database.DB) {
 		cmdParts := strings.SplitN(listen.Text(), " ", 3)
 		switch cmd := cmdParts[0]; cmd {
 		case "quit":
+			fallthrough
+		case "q":
 			return
 		case "write":
+			fallthrough
+		case "w":
 			if len(cmdParts) < 3 {
 				fmt.Println("missing the key and/or value arguments; try 'write <key> <value>'.")
 				break
@@ -34,13 +38,15 @@ func displayInterface(db database.DB) {
 			db = db.Write(k, v)
 			fmt.Println("written")
 		case "read":
+			fallthrough
+		case "r":
 			if len(cmdParts) < 2 {
 				fmt.Println("missing the key argument; try 'read <key>'.")
 				break
 			}
 			k, v := cmdParts[1], ""
 			db = db.Read(k, &v)
-			fmt.Printf("%s = %s\n", k, v)
+			fmt.Printf("%s: %s\n", k, v)
 		case "debug":
 			if len(cmdParts) < 2 {
 				fmt.Println("missing the key argument; try 'debug <key>'.")
@@ -52,10 +58,10 @@ func displayInterface(db database.DB) {
 			db = db.Reindex()
 		default:
 			fmt.Println(`Command Help:
-  quit 	              : Quits the application
-  write <key> <value> : Writes the value to the key
-  read <key>          : Returns the value for key
-  reindex             : Rebuilds the database index`)
+  q(uit)                : Quits the application
+  w(rite) <key> <value> : Writes the value to the key
+  r(ead) <key>          : Returns the value for key
+  reindex               : Rebuilds the database index`)
 		}
 		fmt.Print("> ")
 	}
